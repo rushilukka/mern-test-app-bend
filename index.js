f257d0a23555165024ab998fe5d15cc07fa8c317
Rushi N Lukka <134401284+rushilukka@users.noreply.github.com>
2024-04-08 17:24:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.use(cors({
-    origin:["http://localhost:3001",""]
-}));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/sample')
+mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
