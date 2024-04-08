3c9b2f2b298ba031389fea9a21454c57e75f5625
Rushi N Lukka <134401284+rushilukka@users.noreply.github.com>
2024-04-08 17:36:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config()
+//require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 5001;
